8950b1e406c8fa7b6b8561245d910aebc8b8cde7
New 6969 <94561475+AayushMainali-Github@users.noreply.github.com>
2024-03-24 17:23:59 +0545

    test

--- index.py
+++ index.py
@@ -7,11 +7,16 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
+# ["email","password","username","displayname"]
 @app.route("/register", methods= ["POST"])
 def register():
     user_data = json.loads(request.data)
-    print(user_data["password"])
-    print(strength_checker(user_data["password"]))
+    email = user_data["email"]
+    # password = user_data["password"]
+    if "password" and "email" in user_data.keys():
+        print("test")
+    # print(strength_checker(password))
     return jsonify({"message":"User Registered Successfully"}), 200
 
 
@@ -21,11 +26,13 @@ def strength_checker(password_to_check):
             return {"status":False, "message":"Your password should be at least 8 characters long."}
         if sum(1 for c in password_to_check if c.isupper()) <= 0:
              return {"status":False, "message":"Use at least one capital letters."}
-        if sum(1 for c in password_to_check if isinstance(c, int)) <=0:
+        if sum(1 for c in password_to_check if c.isdigit()) <=0:
             return {"status":False, "message":"Use at least one number"}
         return {"status":True}
 
 def mail_verifier(email_to_verify):
      if sum(1 for c in email_to_verify if c=='@') != 1:
-          return jsonify({"message":"Invalid email"}),200
-     return True
+         return {"status":False, "message":"Invalid Email"}
+     if sum(1 for c in email_to_verify if c=='.') != 1:
+         return {"status":False, "message":"Invalid Email"}
+     return {"status":True}
