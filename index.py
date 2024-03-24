e76ccb66355be90f48205d490e1e28b2e0a6f417
CheckeredKnight <tprasiddha98@gmail.com>
2024-03-24 17:03:22 +0545

    email verifier and password strength checker

--- index.py
+++ index.py
@@ -11,3 +11,19 @@ def hello_world():
 def register():
     user_data = request.get_json()
     return jsonify({"message":"User Registered Successfully"}), 200
+
+def strength_checker(user_data):
+        password_to_check = user_data.password
+        if len(password_to_check) < 8:
+            return jsonify({"message":"Your password should be at least 8 characters long."}),200
+        if sum(1 for c in password_to_check if c.isupper()) <= 0:
+             return jsonify({"message":"Use at least one capital letters."}), 200
+        if sum(1 for c in password_to_check if isinstance(c, int)) <=0:
+            return jsonify({"message":"Use at least one number"})
+        return True, 200
+
+def mail_verifier(user_data):
+     email_to_verify = user_data.email
+     if sum(1 for c in email_to_verify if c=='@') != 1:
+          return jsonify({"message":"Invalid email"}),200
+     return True
