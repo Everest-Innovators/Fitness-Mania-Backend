7355e67bd3b4050f80c4b9cbfb1726772c92909a
New 6969 <94561475+AayushMainali-Github@users.noreply.github.com>
2024-03-24 16:38:26 +0545

    post request

--- index.py
+++ index.py
@@ -1,7 +1,14 @@
-from flask import Flask
+import json
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/register", methods= ["POST"])
+def register():
+    data = request.data
+    print(data)
+    return jsonify({"message":"User Registered Successfully"}), 200
