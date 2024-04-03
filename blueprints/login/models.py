3d9d0747f7bb15fa1dfc6621b431297f3af9b883
New 6969 <94561475+AayushMainali-Github@users.noreply.github.com>
2024-04-03 22:15:07 +0545

    Check if valid email

--- blueprints/login/models.py
+++ blueprints/login/models.py
@@ -3,6 +3,8 @@ import bcrypt
 
 def fetch_user(password, email):
     cursor.execute("SELECT password FROM users WHERE email = %s", (email,))
+    if(not cursor.fetchone()):
+        return False
     hashedPass = cursor.fetchone()[0]
     if not bcrypt.checkpw(password.encode('utf8'),hashedPass.encode('utf8')):
         return False
