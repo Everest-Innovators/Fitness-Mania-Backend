6e9250d7b09535372245aac2a4494ef89c322619
New 6969 <94561475+AayushMainali-Github@users.noreply.github.com>
2024-04-03 22:18:14 +0545

    Another minor bug

--- blueprints/login/models.py
+++ blueprints/login/models.py
@@ -3,9 +3,10 @@ import bcrypt
 
 def fetch_user(password, email):
     cursor.execute("SELECT password FROM users WHERE email = %s", (email,))
-    if(not cursor.fetchone()):
+    data = cursor.fetchone()
+    if(not data):
         return False
-    hashedPass = cursor.fetchone()[0]
+    hashedPass = data[0]
     if not bcrypt.checkpw(password.encode('utf8'),hashedPass.encode('utf8')):
         return False
     cursor.execute("SELECT id FROM users WHERE email = %s", (email,))
